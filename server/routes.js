82dcc86b5b3bef57f00811657c5abf47761c8129
María V. Ortiz <mariatua2@gmail.com>
2020-07-21 16:35:35 -0400

    Fix: perfumer controller bug

--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const perfumeController = require('./controllers/perfumeController')
 
 router.get('/ping', (req, res) => {return res.send('pong')}) //test
-router.get('/perfume/list', perfumeController.perfumeList); //lista de perfumes
-router.post('/perfume/add', perfumeController.perfumeAdd);
-router.post('/perfume/edit', perfumeController.perfumeEdit);
-router.post('/perfume/delete', perfumeController.perfumeDelete);
-router.post('/select/dictionary', perfumeController.selectDictionary);
-router.post('/perfume/read', perfumeController.perfumeRead);
+
 
 
 
