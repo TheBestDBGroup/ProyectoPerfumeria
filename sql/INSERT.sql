e9a89e7e6a249746a332535952a9c8ffa58c3e00
María V. Ortiz <mariatua2@gmail.com>
2020-07-22 19:38:34 -0400

    Fix(verificar): Error en inserts

--- sql/INSERT.sql
+++ sql/INSERT.sql
@@ -342,9 +342,8 @@ INSERT INTO ydm_contrato VALUES
 INSERT INTO ydm_renueva VALUES
   (DEFAULT, 1, '2020/07/05'),
   (DEFAULT, 5, '2019/10/12'),
-  (DEFAULT, 8, '2019/12/14'),
-  (DEFAULT, 14,'2019/08/05');  
-  
+  (DEFAULT, 8, '2019/12/14');
+
 INSERT INTO ydm_condicion_pago VALUES
   (DEFAULT, 1, 'Contado', NULL, NULL, NULL),
   (DEFAULT, 2, 'Crédito', 2, 50, 6),
