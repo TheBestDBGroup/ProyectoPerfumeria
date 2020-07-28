173fafb839493c41cc82cf2975815b2ea1ba260a
Yorfrank Bastidas <ykbastidasc@gmail.com>
2020-07-28 15:01:23 -0400

    Finalizar recomendador

--- server/controllers/recomendadorController.js
+++ server/controllers/recomendadorController.js
@@ -17,6 +17,9 @@ const getPerfumesRecomendador = (request, response) => {
     request.body.edad,
     request.body.intensidad,
     request.body.caracter,
+    request.body.aroma,
+    request.body.preferencia,
+    request.body.personalidad,
   ];
   const queryPerfumesRecomendador =
     "SELECT pe.id_perfume, pe.nombre_perfume, 'Familia olfativa' as tipo_palabra,\
@@ -61,7 +64,7 @@ const getPerfumesRecomendador = (request, response) => {
     \
     UNION\
     \
-    SELECT pe.id_perfume, pe.nombre_perfume, 'Carácter' as tipo_palabra, pc.nombre_palabra_clave as palabra_clave, MAX(cuenta.ocurrencia)\
+    SELECT pe.id_perfume, pe.nombre_perfume, 'Carácter' as tipo_palabra, pc.nombre_palabra_clave as palabra_clave, cuenta.ocurrencia\
     FROM ydm_perfume pe, ydm_palabra_clave pc, ydm_fo_pc fp, ydm_principal pr, ydm_familia_olfativa fo,\
       (SELECT id_perfume, count (*) as ocurrencia\
       FROM ydm_perfume, ydm_palabra_clave, ydm_fo_pc, ydm_principal, ydm_familia_olfativa\
@@ -72,7 +75,54 @@ const getPerfumesRecomendador = (request, response) => {
     WHERE pe.id_perfume = pr.id_perfume_principal AND pr.id_familia_olfativa_principal = fo.id_familia_olfativa\
     AND fo.id_familia_olfativa = fp.id_familia_olfativa_fo_pc AND fp.id_palabra_clave_fo_pc = pc.id_palabra_clave\
     AND nombre_palabra_clave = ANY ($14) AND fragrancia_principal = true\
-    GROUP BY pe.id_perfume, pe.nombre_perfume, pc.nombre_palabra_clave\
+    AND cuenta.id_perfume = pe.id_perfume\
+    \
+    UNION\
+    \
+    SELECT pe.id_perfume, pe.nombre_perfume, 'Aroma' as tipo_palabra, pc.nombre_palabra_clave as palabra_clave, cuenta.ocurrencia\
+    FROM ydm_perfume pe, ydm_palabra_clave pc, ydm_fo_pc fp, ydm_principal pr, ydm_familia_olfativa fo,\
+      (SELECT id_perfume, count (*) as ocurrencia\
+      FROM ydm_perfume, ydm_palabra_clave, ydm_fo_pc, ydm_principal, ydm_familia_olfativa\
+      WHERE id_perfume = id_perfume_principal AND id_familia_olfativa_principal = id_familia_olfativa\
+      AND id_familia_olfativa = id_familia_olfativa_fo_pc AND id_palabra_clave_fo_pc = id_palabra_clave\
+      AND nombre_palabra_clave = ANY ($15) AND fragrancia_principal = true\
+      GROUP BY id_perfume) cuenta\
+    WHERE pe.id_perfume = pr.id_perfume_principal AND pr.id_familia_olfativa_principal = fo.id_familia_olfativa\
+    AND fo.id_familia_olfativa = fp.id_familia_olfativa_fo_pc AND fp.id_palabra_clave_fo_pc = pc.id_palabra_clave\
+    AND nombre_palabra_clave = ANY ($15) AND fragrancia_principal = true\
+    AND cuenta.id_perfume = pe.id_perfume\
+    \
+    UNION\
+    \
+    SELECT pe.id_perfume, pe.nombre_perfume, 'Preferencia uso' as tipo_palabra,\
+    pc.nombre_palabra_clave as palabra_clave, cuenta.ocurrencia\
+    FROM ydm_perfume pe, ydm_palabra_clave pc, ydm_fo_pc fp, ydm_principal pr, ydm_familia_olfativa fo,\
+      (SELECT id_perfume, count (*) as ocurrencia\
+      FROM ydm_perfume, ydm_palabra_clave, ydm_fo_pc, ydm_principal, ydm_familia_olfativa\
+      WHERE id_perfume = id_perfume_principal AND id_familia_olfativa_principal = id_familia_olfativa\
+      AND id_familia_olfativa = id_familia_olfativa_fo_pc AND id_palabra_clave_fo_pc = id_palabra_clave\
+      AND nombre_palabra_clave = ANY ($16) AND fragrancia_principal = true\
+      GROUP BY id_perfume) cuenta\
+    WHERE pe.id_perfume = pr.id_perfume_principal AND pr.id_familia_olfativa_principal = fo.id_familia_olfativa\
+    AND fo.id_familia_olfativa = fp.id_familia_olfativa_fo_pc AND fp.id_palabra_clave_fo_pc = pc.id_palabra_clave\
+    AND nombre_palabra_clave = ANY ($16) AND fragrancia_principal = true \
+    AND cuenta.id_perfume = pe.id_perfume\
+    \
+    UNION\
+    \
+    SELECT pe.id_perfume, pe.nombre_perfume, 'Personalidad' as tipo_palabra,\
+    pc.nombre_palabra_clave as palabra_clave, cuenta.ocurrencia\
+    FROM ydm_perfume pe, ydm_palabra_clave pc, ydm_fo_pc fp, ydm_principal pr, ydm_familia_olfativa fo,\
+      (SELECT id_perfume, count (*) as ocurrencia\
+      FROM ydm_perfume, ydm_palabra_clave, ydm_fo_pc, ydm_principal, ydm_familia_olfativa\
+      WHERE id_perfume = id_perfume_principal AND id_familia_olfativa_principal = id_familia_olfativa\
+      AND id_familia_olfativa = id_familia_olfativa_fo_pc AND id_palabra_clave_fo_pc = id_palabra_clave\
+      AND nombre_palabra_clave = ANY ($17) AND fragrancia_principal = true\
+      GROUP BY id_perfume) cuenta\
+    WHERE pe.id_perfume = pr.id_perfume_principal AND pr.id_familia_olfativa_principal = fo.id_familia_olfativa\
+    AND fo.id_familia_olfativa = fp.id_familia_olfativa_fo_pc AND fp.id_palabra_clave_fo_pc = pc.id_palabra_clave\
+    AND nombre_palabra_clave = ANY ($17) AND fragrancia_principal = true \
+    AND cuenta.id_perfume = pe.id_perfume\
     ORDER BY id_perfume, tipo_palabra, palabra_clave";
   pool.query(
     queryPerfumesRecomendador,
